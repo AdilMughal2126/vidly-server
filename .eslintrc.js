438b7fcd7fc7b89a7b9608b065cf390fe1da3094
takanome_dev <takanomedev221@gmail.com>
2022-01-02 16:16:00 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: ["./tsconfig.json"] },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
---
+++ server.ts
@@ -0,0 +1,7 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => res.send("Hello World"));
+
+app.listen(5000, () => console.log(`Server is running`));
