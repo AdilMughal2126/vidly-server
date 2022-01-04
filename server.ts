6a95c1535af6c95db99e8c7f889795db47be02f7
takanome_dev <takanomedev221@gmail.com>
2022-01-04 10:14:35 +0000

    refactor: update server.ts

--- server.ts
+++ server.ts
@@ -3,12 +3,13 @@ import express from "express";
 import helmet from "helmet";
 import Debug from "debug";
 import dotenv from "dotenv";
-import mongoose, { Mongoose } from "mongoose";
-
-const debugDB = Debug("Express:Database:Connection");
+import mongoose from "mongoose";
 
 import { genres } from "./routes/genres";
 import { customers } from "./routes/customers";
+
+const debugDB = Debug("Express:Database:Connection");
+const debugConsole = Debug("Express:Server:Running");
 
 const app = express();
 
@@ -16,7 +17,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const connect: Mongoose = await mongoose.connect(process.env.MONGO_URI!);
+    const connect = await mongoose.connect(process.env.MONGO_URI!);
     const { host, port, name } = connect.connection;
     debugDB(`MongoDB Connected: ${host}:${port}/${name}`);
   } catch (err) {
@@ -24,14 +25,14 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
 void connectDB();
 
 app.use(helmet());
 app.use(express.json());
+
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
-
-const debugConsole = Debug("Express:Server:Running");
 
 app.listen(process.env.PORT, () =>
   debugConsole(
