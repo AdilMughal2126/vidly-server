092ed6dff671ccd690cf8fbd4f86a724946c487f
takanome_dev <takanomedev221@gmail.com>
2022-02-04 19:57:21 +0000

    [refactor]: update db connection

--- db/db.ts
+++ db/db.ts
@@ -3,14 +3,13 @@ import { logger } from "../helpers/logger";
 
 export const connectDB = async () => {
 	try {
-		// let connect: typeof mongoose;
+		let connect: typeof mongoose;
 
-		// if (process.env.NODE_ENV === "test") {
-		// 	connect = await mongoose.connect(process.env.MONGO_URI_TEST as string);
-		// } else {
-		// 	connect = await mongoose.connect(process.env.MONGO_URI as string);
-		// }
-		const connect = await mongoose.connect("mongodb://localhost/vidly");
+		if (process.env.NODE_ENV === "test") {
+			connect = await mongoose.connect(process.env.MONGO_URI_TEST as string);
+		} else {
+			connect = await mongoose.connect(process.env.MONGO_URI as string);
+		}
 
 		const { host, port, name } = connect.connection;
 		logger.info(`MongoDB Connected: ${host}:${port}/${name}`);
