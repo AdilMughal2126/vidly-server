ea4b2cbae178089eb5211f99f9402bd520cbcf2b
takanome_dev <takanomedev221@gmail.com>
2022-02-22 20:47:51 +0000

    [feat]: update user route

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,61 +1,61 @@
-import _ from "lodash";
-import { User } from "../models/user";
 import { Request, Response } from "express";
-import { UserType } from "../types/UserType";
-import { Params } from "../types/ParamsType";
-import { asyncMiddleware } from "../middleware/async";
+import _ from "lodash";
 import { generateAuthToken, generateHash } from "../helpers/auth";
+import { asyncMiddleware } from "../middleware/async";
+import { User } from "../models/user";
+import { Params } from "../types/ParamsType";
+import { UserType } from "../types/UserType";
 
 export const handleGetUsers = asyncMiddleware(
-  async (req: Request, res: Response) => {
-    const users = await User.find().sort("name").select("-hash");
-    return res.json(users);
-  }
+	async (req: Request, res: Response) => {
+		const users = await User.find().sort("name").select("-hash");
+		return res.json(users);
+	}
 );
 
 export const handleGetUser = asyncMiddleware(
-  async (req: Request, res: Response) => {
-    const user = await User.findById(req.params.id).select("-hash");
-    if (!user) return res.status(404).json("User Not Found");
-    return res.json(user);
-  }
+	async (req: Request, res: Response) => {
+		const user = await User.findById(req.params.id).select("-hash");
+		if (!user) return res.status(404).json("User Not Found");
+		return res.json(user);
+	}
 );
 
 // TODO: Get rid of lodash method
 export const handleCreateUser = asyncMiddleware(
-  async (req: Request<unknown, unknown, UserType>, res: Response) => {
-    const { name, email } = req.body;
-    const isEmail = await User.findOne({ email });
-    if (isEmail) return res.status(400).json("Email already registered");
-    const hash = await generateHash(req.body.password);
-    const user = await User.create({ name, email, hash });
-    const token = generateAuthToken(user);
-    return res
-      .header("X-Auth-Token", token)
-      .header("Access-Control-Expose-Headers", "X-Auth-Token")
-      .json(_.pick(user, ["_id", "name", "email"]));
-  }
+	async (req: Request<unknown, unknown, UserType>, res: Response) => {
+		const { name, email } = req.body;
+		const isEmail = await User.findOne({ email });
+		if (isEmail) return res.status(400).json("Email already registered");
+		const hash = await generateHash(req.body.password);
+		const user = await User.create({ name, email, hash });
+		const token = generateAuthToken(user);
+		return res
+			.header("X-Auth-Token", token)
+			.header("Access-Control-Expose-Headers", "X-Auth-Token")
+			.json(_.pick(user, ["_id", "name", "email"]));
+	}
 );
 
 // TODO: Handle Password Update
 export const handleUpdateUser = asyncMiddleware(
-  async (req: Request<Params, unknown, UserType>, res: Response) => {
-    const { name, email } = req.body;
-    const hash = await generateHash(req.body.password);
-    const user = await User.findByIdAndUpdate(
-      req.params.id,
-      { name, email, hash },
-      { new: true }
-    );
-    if (!user) return res.status(404).json("User Not Found");
-    return res.json(user);
-  }
+	async (req: Request<Params, unknown, UserType>, res: Response) => {
+		const { name, email } = req.body;
+		const user = await User.findByIdAndUpdate(
+			req.params.id,
+			{ $set: { name, email } },
+			{ new: true }
+		);
+		if (!user) return res.status(404).json("User not found");
+		const token = generateAuthToken(user);
+		return res.json(token);
+	}
 );
 
 export const handleDeleteUser = asyncMiddleware(
-  async (req: Request, res: Response) => {
-    const user = await User.findByIdAndRemove(req.params.id);
-    if (!user) return res.status(404).json("User Not Found");
-    return res.json(user);
-  }
+	async (req: Request, res: Response) => {
+		const user = await User.findByIdAndRemove(req.params.id);
+		if (!user) return res.status(404).json("User not found");
+		return res.json(user);
+	}
 );
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,25 +1,21 @@
 import express from "express";
 import {
-  handleCreateUser,
-  handleDeleteUser,
-  handleGetUser,
-  handleGetUsers,
-  handleUpdateUser,
+	handleCreateUser,
+	handleDeleteUser,
+	handleGetUser,
+	handleGetUsers,
+	handleUpdateUser,
 } from "../controllers/users";
-import { validateUser } from "../models/user";
-import { requireAdmin } from "../middleware/auth";
+import { requireAdmin, requireAuth } from "../middleware/auth";
 import { validateId } from "../middleware/validateObjectId";
 import { validateRequest } from "../middleware/validateRequest";
+import { validateUser } from "../models/user";
 
 const router = express.Router();
 router.route("/").get(handleGetUsers);
 router.get("/:id", validateId, handleGetUser);
 router.post("/", validateRequest(validateUser), handleCreateUser);
-router.put(
-  "/:id",
-  [validateId, requireAdmin, validateRequest(validateUser)],
-  handleUpdateUser
-);
+router.put("/:id", [validateId, requireAuth], handleUpdateUser);
 router.delete("/:id", [validateId, requireAdmin], handleDeleteUser);
 
 export { router as users };
