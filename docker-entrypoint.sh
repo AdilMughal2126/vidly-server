d2bbf241e9e5df1b2b5d3d7142bc1fc492e69673
takanome_dev <takanomedev221@gmail.com>
2022-02-23 16:24:09 +0000

    [refactor]: update docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "Waiting for MongoDB to start..."
-./wait-for.sh db:27017
+echo 'Waiting for MongoDB to start...'
+./wait-for-it.sh db:27017
 
-echo "Inserting seed-data to the database..."
-npm run seed
+echo 'Inserting seed-data to the database...'
+yarn run seed
 
-echo "Starting dev server...."
-npm run dev
+echo 'Starting dev server....'
+yarn run dev
